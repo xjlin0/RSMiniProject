9c7f639bf22d15113732b99337bd9a07a483b1f3
Jack <xjlin0@gmail.com>
2015-06-04 17:03:23 -0700

    setup initial rails framwork

---
+++ app/models/items_user.rb
@@ -0,0 +1,4 @@
+class ItemsUser < ActiveRecord::Base
+  belongs_to :item
+  belongs_to :user
+end
---
+++ spec/models/items_user_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ItemsUser, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
